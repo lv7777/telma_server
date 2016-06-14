09bb4f25c58853bb005cebfc4d6e504ab4f049b3
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-06-13 18:17:29 -0700

    supertestが機能してなかったので雛形作った。

---
+++ test/supertesttest.js
@@ -0,0 +1,45 @@
+// const assert = require("assert");
+// const request = require("supertest");
+// const express=require("express")
+// const router = require("../supertestlearn.js");;
+// let app=express()
+// app.use(router("tes"));
+// app.listen(3000);
+// describe("GET /chat", () => {
+//     it("response is json and format is true", (done) => {
+//         const data = "";
+//         //   request(router).get("/chat").expect(200,done);
+
+
+//         request(app)
+//             .get('/user')
+//             .expect('Content-Type', /json/)
+//             .expect('Content-Length', '15')
+//             .expect(200)
+//             .end(function (err, res) {
+//                 if (err) throw err;
+//             });
+//     });
+// });
+const assert = require("assert");
+var request = require('supertest')
+    , express = require('express');
+
+var app = express();
+
+app.get('/user', function (req, res) {
+    res.status(200).json({ name: 'tobi' });
+});
+
+
+describe("GET /chat", () => {
+    it("response is json and format is true", (done) => {
+        request(app)
+            .get('/user')
+            .expect('Content-Type', /json/)
+            .expect('Content-Length', '15')
+            .expect(200)
+            .expect(200,done);
+
+    })
+});
